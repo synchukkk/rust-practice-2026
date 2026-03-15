945bd3a905ee2798a647b32f66289985e944bf29
synchukkk <yaroslav.synchuk@e-u.edu.ua>
2026-03-15 18:30:23 +0200

    Add kangaroo function with test cases
    Implement kangaroo function to determine if two kangaroos meet.

--- src/hackerrank/task06.rs
+++ src/hackerrank/task06.rs
@@ -1,1 +1,38 @@
-// https://www.hackerrank.com/challenges/kangaroo/problem
+fn kangaroo(x1: i32, v1: i32, x2: i32, v2: i32) -> String {
+    if v1 == v2 {
+        if x1 == x2 {
+            "YES".to_string()
+        } else {
+            "NO".to_string()
+        }
+    } else if (x2 - x1) % (v1 - v2) == 0 && (x2 - x1) / (v1 - v2) > 0 {
+        "YES".to_string()
+    } else {
+        "NO".to_string()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_yes() {
+        assert_eq!(kangaroo(0, 3, 4, 2), "YES");
+    }
+
+    #[test]
+    fn test_no() {
+        assert_eq!(kangaroo(0, 2, 5, 3), "NO");
+    }
+
+    #[test]
+    fn test_same_speed_same_position() {
+        assert_eq!(kangaroo(0, 3, 0, 3), "YES");
+    }
+
+    #[test]
+    fn test_same_speed_different_position() {
+        assert_eq!(kangaroo(0, 3, 5, 3), "NO");
+    }
+}
