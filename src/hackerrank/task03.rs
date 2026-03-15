15589ef7db798e0cabeb987d21a55800f3855c80
synchukkk <yaroslav.synchuk@e-u.edu.ua>
2026-03-15 17:39:58 +0200

    Implement staircase function with tests

--- src/hackerrank/task03.rs
+++ src/hackerrank/task03.rs
@@ -1,1 +1,32 @@
-// https://www.hackerrank.com/challenges/staircase/problem
+fn staircase(n: i32) {
+    for i in 1..=n {
+        let spaces = n - i;
+        let hashes = i;
+        println!("{}{}", " ".repeat(spaces as usize), "#".repeat(hashes as usize));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_staircase_4() {
+        // n=4:
+        //    #
+        //   ##
+        //  ###
+        // ####
+        staircase(4);
+    }
+
+    #[test]
+    fn test_staircase_1() {
+        staircase(1);
+    }
+
+    #[test]
+    fn test_staircase_6() {
+        staircase(6);
+    }
+}
